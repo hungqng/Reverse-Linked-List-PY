3df477e91db3262369d28f7552a2a8dbae5795f2
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-04-13 12:40:00 -0700

    Add files via upload

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -16,3 +16,11 @@ class Solution:
             curr.next = prev
             prev = curr
         return prev
+
+        # Recursion
+        # if not head or not head.next:
+        #     return head
+        # node = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+        # return node
